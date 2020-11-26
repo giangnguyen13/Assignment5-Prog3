a6e1d065216a39ba4520cb4b8c0f03294f3156c8
Giang Nguyen <giangkawaii1999@gmail.com>
2020-11-26 11:45:40 -0500

    set up csv reading

--- Assignment5-Prog3/MainWindow.xaml.cs
+++ Assignment5-Prog3/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using LumenWorks.Framework.IO.Csv;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +26,77 @@ namespace Assignment5_Prog3
         public MainWindow()
         {
             InitializeComponent();
+
+            //Console.WriteLine("Hello world");
+            try
+            {
+                ReadCsv(@"H:\Programming3-project\biostats.csv");
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.Message);
+            }
+            //Console.ReadKey();
+        }
+
+        static void ReadCsv(string fileLocation)
+        {
+            //// open the file "data.csv" which is a CSV file without headers
+            //using (var csv = new CachedCsvReader(new StreamReader("data.csv"), false))
+            //{
+            //    csv.Columns.Add(new Column { Name = "PriceDate", Type = typeof(DateTime) });
+            //    csv.Columns.Add(new Column { Name = "OpenPrice", Type = typeof(decimal) });
+            //    csv.Columns.Add(new Column { Name = "HighPrice", Type = typeof(decimal) });
+            //    csv.Columns.Add(new Column { Name = "LowPrice", Type = typeof(decimal) });
+            //    csv.Columns.Add(new Column { Name = "ClosePrice", Type = typeof(decimal) });
+            //    csv.Columns.Add(new Column { Name = "Volume", Type = typeof(int) });
+
+            //    // Field headers will now be picked from the Columns collection
+            //    myDataGrid.DataSource = csv;
+            //}
+            var csvTable = new DataTable();
+            using (var csvReader = new CsvReader(new StreamReader(File.OpenRead(fileLocation)), true))
+            {
+                csvTable.Load(csvReader);
+            }
+            //foreach (var column in csvTable.Columns) 
+            //{
+            //    string Column1 = column.ToString();
+            //    Console.WriteLine(Column1);
+            //}
+
+            //foreach (var row in csvTable.Rows)
+            //{
+            //    foreach (var column in csvTable.Columns)
+            //    {
+            //        string Column1 = column.ToString();
+            //        Console.Write(Column1 + ",");
+            //    }
+            //    Console.WriteLine("");
+            //}
+
+            for (int i = 0; i < 2/*csvTable.Rows.Count*/; i++)
+            {
+                string x = "";
+                for (int j = 0; j < csvTable.Columns.Count; j++)
+                {
+                    x += csvTable.Rows[i][j].ToString() + ",";//Console.Write();
+                }
+
+                if (true)//negative row
+                {
+                    //remove 
+                }
+                //Console.WriteLine("");
+                MessageBox.Show(x);
+            }
+
+
+            // Similarly we can access the rows of the .csv file as ,
+
+            //string Row1 = csvTable.Rows[0][0].ToString();
+            //Console.WriteLine(Column1);
+            //Console.WriteLine(Row1);
         }
     }
 }
